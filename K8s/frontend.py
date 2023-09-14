e382c3dd7297481a58df4c622683401ba1d33837
Dmitriy Voronkov <58822703+Mitya178@users.noreply.github.com>
2023-09-14 20:01:55 +0300

    Update frontend.py

--- K8s/frontend.py
+++ K8s/frontend.py
@@ -4,8 +4,7 @@ import os
 
 app = Flask(__name__)
 app.template_folder = '/opt/MTS_Task'
-backend1 = os.getenv('BACKEND1')
-backend2 = os.getenv('BACKEND2')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
@@ -14,11 +13,11 @@ def homepage():
         num2 = int(request.form['num2'])
 
         # Вычисление суммы чисел
-        sum_response = requests.post(f'http://{backend1}:5051/sum', json={'num1': num1, 'num2': num2})
+        sum_response = requests.post(f'http://backend1-service:5051/sum', json={'num1': num1, 'num2': num2})
         sum_result = sum_response.json()
 
         # Вычисление перемножения чисел
-        multiply_response = requests.post(f'http://{backend2}:5052/multiply', json={'num1': num1, 'num2': num2})
+        multiply_response = requests.post(f'http://backend2-service:5052/multiply', json={'num1': num1, 'num2': num2})
         multiply_result = multiply_response.json()
 
         return render_template('result.html', sum_result=sum_result, multiply_result=multiply_result)
