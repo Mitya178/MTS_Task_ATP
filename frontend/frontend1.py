fcf476b73ed7941a1253056e44649423c6e6e9ee
Dmitriy Voronkov <58822703+Mitya178@users.noreply.github.com>
2023-09-14 10:50:59 +0300

    Create frontend1.py

---
+++ frontend/frontend1.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import requests
+import os
+
+app = Flask(__name__)
+app.template_folder = '/opt/MTS_Task'
+backend1 = os.getenv('BACKEND1')
+backend2 = os.getenv('BACKEND2')
+
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    if request.method == 'POST':
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+
+        # Вычисление суммы чисел
+        sum_response = requests.post('http://172.17.0.2:5051/sum', json={'num1': num1, 'num2': num2})
+        sum_result = sum_response.json()
+
+        # Вычисление перемножения чисел
+        multiply_response = requests.post('http://172.17.0.3:5052/multiply', json={'num1': num1, 'num2': num2})
+        multiply_result = multiply_response.json()
+
+        return render_template('result.html', sum_result=sum_result, multiply_result=multiply_result)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5050)
